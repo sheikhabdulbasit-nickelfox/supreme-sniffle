e13d0f07942c1536995244aa99c113596942abdd
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-04-13 16:57:28 +0530

    boilerplate code

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.sectionContainer}>
+      <Text>abcd</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+});
+
+export default App;
