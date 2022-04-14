25118b28445f1cdd762d9fb5c90628bd3e4a3614
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-04-14 12:14:11 +0530

    chore(MainActivity.java): add changes for navigation

--- android/app/src/main/java/com/goodfood/MainActivity.java
+++ android/app/src/main/java/com/goodfood/MainActivity.java
@@ -1,5 +1,5 @@
 package com.goodfood;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -23,6 +23,11 @@ public class MainActivity extends ReactActivity {
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new MainActivityDelegate(this, getMainComponentName());
   }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+}
 
   public static class MainActivityDelegate extends ReactActivityDelegate {
     public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
