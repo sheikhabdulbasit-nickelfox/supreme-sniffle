4169d9ac826a02e6bdcac2b9b56a3ad9ae7bd6f7
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-04-14 12:16:01 +0530

    feat(SignUp): create a sign up page

---
+++ Source/Screens/SignUp.js
@@ -0,0 +1,12 @@
+import {View, Text, ScrollView} from 'react-native';
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    <ScrollView>
+      <Text>Create New Account</Text>
+    </ScrollView>
+  );
+};
+
+export default SignUp;
